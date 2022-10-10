0b6596a7d02c38a46d917ad387a9153d4a4571a9
sanojkumaryadav <sanojsanoj638@gmail.com>
2022-10-11 01:57:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Main from "./components/Main"
+import React from "react"
+import {useEffect} from "react"
+
+
+function App() {
+
+  return(  
+           <div>
+                <Main />
+           </div>
+       )
+}
+
+export default App;
---
+++ src/components/PersonName2.jsx
@@ -0,0 +1,25 @@
+const PersonName2 = ({data}) => {
+    return (
+        <div style = {{
+            width : "25%",
+            display: "flex",
+            flexDirection : "row-reverse",
+        // border : "1px solid red"
+
+            }}> 
+          <div style = {{
+               width : "200px",
+               textAlign: "center",
+               paddingTop : "20%",
+            //    border : "1px solid green"
+              
+          }}>  
+           {   
+             data[0].Name
+          }
+          </div>
+        </div>
+      )
+}
+
+export default PersonName2
